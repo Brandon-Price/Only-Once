8f9ddf0d695ff94f42dc5106a373fec7ee7db0f2
Brandon-Price <brandon.samuel.price@gmail.com>
2024-09-01 10:04:35 -0500

    Add skeleton for svg checks

--- only-once/src/App.tsx
+++ only-once/src/App.tsx
@@ -1,34 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const user: string = 'yes';
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {/* Checks to see if the user has a cookie, if they do change the svg */}
+      (user === "no" ? (<link id="svg1" rel="icon" type="image/svg+xml" href="/backpack.svg" />))
+      <p>Hello</p>
+    </div>
   )
 }
 
