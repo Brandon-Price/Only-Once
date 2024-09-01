4d0e54a4cfc010f2509fe82fb0dabc639a13ecb7
Brandon-Price <brandon.samuel.price@gmail.com>
2024-09-01 10:28:52 -0500

    Add useEffect for user cookie

--- only-once/src/App.tsx
+++ only-once/src/App.tsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './styles/App.css'
 
 function App() {
 
-  const user: string = 'yes';
+  const user: string = 'hasCookie';
+
+  // Logic that will roughly be used to handle the some of the pages elements
+  useEffect(() => {
+    if(user == 'hasCookie'){
+      const link = document.createElement('link');
+      link.id = 'svg1';
+      link.rel = 'icon';
+      link.type = 'image/svg+xml';
+      link.href = '/alt.svg';
+      document.head.appendChild(link);
+
+      document.title = 'Closed, Sorry';
+    } else {
+      const link = document.createElement('link');
+      link.id = 'svg1';
+      link.rel = 'icon';
+      link.type = 'image/svg+xml';
+      link.href = '/backpack.svg';
+      document.head.appendChild(link);
+
+      document.title = 'Welcome';
+    }
+  }, [user])
 
   return (
     <div>
-      {/* Checks to see if the user has a cookie, if they do change the svg */}
-      (user === "no" ? (<link id="svg1" rel="icon" type="image/svg+xml" href="/backpack.svg" />))
       <p>Hello</p>
     </div>
   )
